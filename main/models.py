33fb8544b809e449d7066a4e90d10375af04dc74
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-03-25 19:42:11 +0100

    Adding more files

--- main/models.py
+++ main/models.py
@@ -2,11 +2,33 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class BlogTag(models.Model):
+    name = models.CharField(verbose_name=_('Title'), max_length=100)
+    created = models.DateTimeField(verbose_name=_('Created Date'), auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(verbose_name=_('Updated Date'), auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Blog Tag')
+        verbose_name_plural = _('Blog Tags')
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name=_('Title'), max_length=100)
+    created = models.DateTimeField(verbose_name=_('Created Date'), auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(verbose_name=_('Updated Date'), auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Blog Category')
+        verbose_name_plural = _('Blog Categories')
+
+
 class BlogPost(models.Model):
     title = models.CharField(verbose_name=_('Title'), max_length=100)
     text = models.TextField(verbose_name=_('Description'), max_length=1000, blank=True)
     created = models.DateTimeField(verbose_name=_('Created Date'), auto_now_add=True, blank=True, null=True)
     updated = models.DateTimeField(verbose_name=_('Updated Date'), auto_now_add=True, blank=True, null=True)
+    categories = models.ManyToManyField(BlogCategory)
+    tags = models.ManyToManyField(BlogTag)
 
     class Meta:
         verbose_name = _('Blog Post')
